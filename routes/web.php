dc8bc260212fe5440011425ed40218f6de615267
林駿綦 <r10101720307@gmail.com>
2020-10-26 19:48:29 +0800

    新增名為 news.index 的 Route，並編輯 NewsController 的 index 方法

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 Route::get('about', [App\Http\Controllers\AboutController::class, 'index'])->name('about.index');
+Route::get('news', [App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
